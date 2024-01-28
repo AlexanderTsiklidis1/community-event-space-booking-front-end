bd9b31013c71c7b0a79b379cb26d25bbe0a833db
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-01-28 18:25:26 -0500

    Add 'useEffect'.

--- Components/BookingDetails.jsx
+++ Components/BookingDetails.jsx
@@ -4,6 +4,19 @@ import { Link, useParams, useNavigate } from 'react-router-dom';
 const API = import.meta.env.VITE_BASE_URL;
 
 const BookingDetails = () => {
+	const [booking, setBooking] = useState([]);
+	const { id } = useParams();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		fetch(`${API}/bookings/${id}`)
+			.then((response) => response.json())
+			.then((responseJSON) => {
+				setBooking(responseJSON);
+			})
+			.catch((error) => console.log(error));
+	}, [id, API]);
+
 	return <div>BookingDetails</div>;
 };
 
