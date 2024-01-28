c225261ea8ad6709c2dc15a80955b3f03e5359bf
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2024-01-28 17:39:50 -0500

    Fix path in app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../Pages/HomePage";
-import MeetingRoomNewPage from "./Pages/MeetingRoomNewPage";
-import FourOFour from "./Pages/FourOFour";
+import MeetingRoomNewPage from "../Pages/MeetingRoomNewPage";
+import FourOFour from "../Pages/FourOFour";
 
 function App() {
   return (
