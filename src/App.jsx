8d08747a30f2824df869758aa1a23279fc673bf0
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2024-01-28 14:35:22 -0500

    Import routes to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from '../Pages/HomePage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "../Pages/HomePage";
+import MeetingRoomNewPage from "./Pages/MeetingRoomNewPage";
+import FourOFour from "./Pages/FourOFour";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-            <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/Bookings/MeetingRoomNewPage"
+            element={<MeetingRoomNewPage />}
+          />
+          <Route path="*" element={<FourOFour />} />
         </Routes>
       </Router>
-      
     </div>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
