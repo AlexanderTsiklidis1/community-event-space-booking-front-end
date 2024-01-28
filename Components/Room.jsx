248d7e8f9c4dd9cebf112ebf6e1140a50b065bf8
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-01-28 16:16:39 -0500

    adds links to room.jsx and booking.jsx

--- Components/Room.jsx
+++ Components/Room.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 function Room({ room }) {
     return (
-        <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', width: '300px' }}>
-            <h3>{room.name}</h3>
-            <p>Capacity: {room.capacity}</p>
-            <p>Floor: {room.floor}</p>
-        </div>
+        <Link to={`/rooms/${room.id}`}> 
+            <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', width: '300px' }}>
+                <h3>{room.name}</h3>
+                <p>Capacity: {room.capacity}</p>
+                <p>Floor: {room.floor}</p>
+            </div>
+        </Link>
     );
 }
 
