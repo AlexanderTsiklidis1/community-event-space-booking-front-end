8b1c02c2e484df93e3f92876d9d19a5e8518e046
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2024-01-28 12:49:23 -0500

    Add NewPage function.

---
+++ Pages/NewPage.jsx
@@ -0,0 +1,12 @@
+import MeetingRoomNewForm from "../Components/MeetingRoomNewForm";
+
+function NewPage() {
+  return (
+    <div className="New">
+      <h2>New</h2>
+      <MeetingRoomNewForm />
+    </div>
+  );
+}
+
+export default NewPage;
