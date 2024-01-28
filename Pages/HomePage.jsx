7310a1fe41fd2f8cdeb21f0b90c7fc5bf8d7dded
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-01-28 07:06:30 -0500

    removes search code from homepage

--- Pages/HomePage.jsx
+++ Pages/HomePage.jsx
@@ -2,27 +2,9 @@ import React, { useState } from 'react';
 import MeetingRoomSearchForm from '../Components/MeetingRoomSearchForm';
 
 function HomePage() {
-  
   const [selectedTab, setSelectedTab] = useState('meetingRooms');
-
-  
-  const [startDate, setStartDate] = useState('');
-  const [startTime, setStartTime] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [endTime, setEndTime] = useState('');
-  const [floor, setFloor] = useState('');
-  const [capacity, setCapacity] = useState('');
-
-  
   const handleTabSelect = (tab) => {
     setSelectedTab(tab);
-  };
-
-  
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-   
-    console.log('Search Submitted:', { startDate, startTime, endDate, endTime, floor, capacity });
   };
 
   return (
