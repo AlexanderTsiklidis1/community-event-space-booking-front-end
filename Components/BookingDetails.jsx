02613aed72b320eaf2faa86bec5b30e23c698c6a
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-01-28 17:29:08 -0500

    Create 'BookingDetails.jsx'.

---
+++ Components/BookingDetails.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+
+const API = import.meta.env.VITE_BASE_URL;
+
+const BookingDetails = () => {
+	return <div>BookingDetails</div>;
+};
+
+export default BookingDetails;
