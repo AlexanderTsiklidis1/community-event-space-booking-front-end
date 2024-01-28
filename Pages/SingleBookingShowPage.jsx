526776e9cba0188c71e88d232722eb6ad7c7a9c9
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2024-01-28 16:59:50 -0500

    Add and rename Single Booking Show Page.

---
+++ Pages/SingleBookingShowPage.jsx
@@ -0,0 +1,12 @@
+import BookingDetails from "../Components/BookingDetails";
+
+function SingleBookingShow() {
+  return (
+    <div className="SingleBooking">
+      <h2>SingleBookingShow</h2>
+      <BookingDetails />
+    </div>
+  );
+}
+
+export default SingleBookingShow;
