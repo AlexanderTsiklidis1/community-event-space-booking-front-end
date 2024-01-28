daab9272a4e711803392eb51f47c3c42b2d1b267
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-01-28 12:28:20 -0500

    Create 'MeetingRoomNewForm.jsx'.

---
+++ Components/MeetingRoomNewForm.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const API = import.meta.env.VITE_BASE_URL;
+
+const MeetingRoomNewForm = () => {
+	return <div>MeetingRoomNewForm</div>;
+};
+
+export default MeetingRoomNewForm;
