5fdcc3037353205584173b8801b9e4b7a10c7b23
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-01-29 18:57:25 -0500

    bug in bookings/roomdetails component

--- Components/Bookings.jsx
+++ Components/Bookings.jsx
@@ -2,68 +2,68 @@ import { useState, useEffect } from "react";
 import Booking from "./Booking";
 import BookingForm from "./BookingForm";
 
-const API = import.meta.env.VITE_API_URL;
+const API = import.meta.env.VITE_BASE_URL;
 
 function Bookings() {
   const [bookings, setBookings] = useState([]);
   useEffect(() => {
-    fetch(`${API}/rooms/${id}/bookings`)
+    fetch(`${API}/bookings`)
       .then(response => response.json())
       .then((responseJSON) => {
         console.log(responseJSON);
-        setBookings(responseJSON.data.payload);
+        setBookings(responseJSON);
       })
       .catch((error) => {
         console.log(error);
       });
-    }, [id,API]);
+    }, []);
 
-    const handleAdd = (newBooking) => {
-      fetch(`${API}/rooms/${id}/bookings`, {
-        method: "POST",
-        body: JSON.stringify(newBooking),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((responseJSON) => {
-          setBookings([...bookings, responseJSON]);
-        })
-        .catch((error) => console.log(error));
-    };
+    // const handleAdd = (newBooking) => {
+    //   fetch(`${API}/rooms/${id}/bookings`, {
+    //     method: "POST",
+    //     body: JSON.stringify(newBooking),
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   })
+    //     .then((response) => response.json())
+    //     .then((responseJSON) => {
+    //       setBookings([...bookings, responseJSON]);
+    //     })
+    //     .catch((error) => console.log(error));
+    // };
   
-    const handleDelete = (bookingId) => {
-      fetch(`${API}/rooms/${id}/bookings/${bookingId}`, { method: "DELETE" })
-        .then((response) => {
-          const copyBookingArray = [...bookings];
-          const indexDeletedBooking = copyBookingArray.findIndex(
-            (booking) => booking.id === bookingId
-          );
-          console.log(reviewId, indexDeletedBooking);
-          copyReviewArray.splice(indexDeletedBooking, 1);
-          console.log("NEW Bookings", copyBookingArray);
-          setReviews(copyBookingArray);
-        })
-        .catch((error) => console.log(error));
-    };
+    // const handleDelete = (bookingId) => {
+    //   fetch(`${API}/rooms/${id}/bookings/${bookingId}`, { method: "DELETE" })
+    //     .then((response) => {
+    //       const copyBookingArray = [...bookings];
+    //       const indexDeletedBooking = copyBookingArray.findIndex(
+    //         (booking) => booking.id === bookingId
+    //       );
+    //       console.log(reviewId, indexDeletedBooking);
+    //       copyReviewArray.splice(indexDeletedBooking, 1);
+    //       console.log("NEW Bookings", copyBookingArray);
+    //       setReviews(copyBookingArray);
+    //     })
+    //     .catch((error) => console.log(error));
+    // };
   
-    const handleEdit = (updatedBooking) => {
-      fetch(`${API}/rooms/${id}/bookings/${updatedBooking.id}`, {
-        method: "PUT",
-        body: JSON.stringify(updatedBooking),
-        headers: { "Content-Type": "application/json" },
-      })
-      .then((response) => response.json())
-      .then(responseJSON => {
-          const copyBookingArray = [...bookings]
-          const indexUpdatedBooking = copyBookingArray.findIndex(booking => {
-              return booking.id === updatedBooking.id
-          })
-          copyBookingArray[indexUpdatedBooking] = responseJSON
-          setBookings(copyBookingArray)
-      });
-    };
+    // const handleEdit = (updatedBooking) => {
+    //   fetch(`${API}/rooms/${id}/bookings/${updatedBooking.id}`, {
+    //     method: "PUT",
+    //     body: JSON.stringify(updatedBooking),
+    //     headers: { "Content-Type": "application/json" },
+    //   })
+    //   .then((response) => response.json())
+    //   .then(responseJSON => {
+    //       const copyBookingArray = [...bookings]
+    //       const indexUpdatedBooking = copyBookingArray.findIndex(booking => {
+    //           return booking.id === updatedBooking.id
+    //       })
+    //       copyBookingArray[indexUpdatedBooking] = responseJSON
+    //       setBookings(copyBookingArray)
+    //   });
+    // };
   return (
     <div className="bookings">
       {bookings.map((booking) => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from '../Pages/HomePage';
-// import RoomNewPage from "/Pages/RoomNewPage";
+import RoomNewPage from "/Pages/RoomNewPage";
 import FourOFour from "../Pages/FourOFourPage";
 import NavBar from "../Components/NavBar";
 import RoomShowPage from "../Pages/RoomShowPage"
+import BookingsPage from "../Pages/BookingsPage";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/rooms/:id" element={<RoomShowPage />} />
-          {/* <Route path="/rooms/new" element={<RoomNewPage />} /> */}
+          <Route path="/bookings" element={<BookingsPage />} />
+          <Route path="/rooms/new" element={<RoomNewPage />} />
           <Route path="*" element={<FourOFour />} />
         </Routes>
       </main>
